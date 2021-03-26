f174e311fd06e15cb257b54a883737e8c119ec5c
harshit0300@gmail.com <harshit0300@gmail.com>
2021-03-26 14:07:31 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Scaffold(
+        body: WebView(
+          initialUrl: "https://www.praveencsrivastava.com/",//My dad's website
+          javascriptMode: JavascriptMode.unrestricted,
+        ),
+      ),
+    ),
+  );
+}
